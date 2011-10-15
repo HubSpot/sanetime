854bfa04fa8046751930f61a335dd51ba0f784f4
prior <prior@cracklabs.com>
2011-10-14 21:49:47 -0400

    Lots of solidness added
    Many more tests
    Dropped down to microseconds for the insides, to make comparisons work
    with datetime

--- sanetime/sanetime.py
+++ sanetime/sanetime.py
@@ -1,13 +1,18 @@
 import calendar as shit_calendar
 from datetime import datetime as fucked_datetime
-from dateutil import parser as ok_parser
+from dateutil import parser as crap_parser
 from error import SaneTimeError
 import pytz
+
+#TODO switch internals all over to microseconds since epoch-- and have millis an seconds as derived from that  (and make sure long's can handle that)
+    # this should make things faster and simpler  - you could have the constructor take an s= or ms= or us= for the different constructor possibillities
+    # okay so int is a long and long is an unlimited, so we only need int
+
 
 class SaneTime(object):
     def __init__(self, *args, **kwargs):
         super(SaneTime,self).__init__()
-        self.utc_millis = None
+        self.utc_micros = None
         self.tz = None
         tzs = []
         args = list(args)
@@ -17,27 +22,39 @@ class SaneTime(object):
         if kwargs.get('timezone'):
             tzs.append(kwargs.pop('timezone').zone)
 
-        if kwargs.get('utc_millis') and len(args)==0:
-            args.insert(0,kwargs.pop('utc_millis'))
+        if kwargs.get('utc_micros') and len(args)==0:
+            args.insert(0,kwargs.pop('utc_micros'))
+        elif kwargs.get('utc_millis') and len(args)==0:
+            args.insert(0,kwargs.pop('utc_millis')*1000)
+        elif kwargs.get('utc_seconds') and len(args)==0:
+            args.insert(0,kwargs.pop('utc_seconds')*1000**2)
             
         if len(args)>2 and len(args)<8:
             args = [fucked_datetime(*args)]
 
+        if len(args)==0:
+            dt = pytz.utc.localize(fucked_datetime.utcnow())
+            args = [shit_calendar.timegm(dt.timetuple())*1000**2+dt.microsecond]
+
         if len(args)==1:
             arg = args[0]
-            if type(arg) in (type(1L),type(1)):
-                self.utc_millis = long(arg)
+            if type(arg) in (type(1L),type(1),type(1.0)):
+                self.utc_micros = int(arg)
             elif type(arg) == type('2000-01-01'):
-                arg = ok_parser.parse(arg)
+                arg = crap_parser.parse(arg)
             if type(arg) == type(fucked_datetime(1,1,1)):
                 dt = arg
                 if dt.tzinfo:
-                    tzs.append(dt.tzinfo.zone)
-                else:
+                    if getattr(dt.tzinfo,'zone',None):
+                        tzs.append(dt.tzinfo.zone)
+                    else:
+                        dt = dt.replace(tzinfo=None)
+                if not dt.tzinfo:
                     dt = pytz.timezone((tzs+['UTC'])[0]).localize(dt)
                 self.__init_from_fucked_datetime(dt)
 
-        if self.utc_millis == None or len(tzs)>1 or len(kwargs)>0:
+
+        if self.utc_micros is None or len(tzs)>1 or len(kwargs)>0:
             raise SaneTimeError('Unexpected constructor args')
 
         tzs.append('UTC')
@@ -48,8 +65,8 @@ class SaneTime(object):
         self.tz = dt.tzinfo.zone
         dt = dt.astimezone(pytz.utc)
         tt = dt.timetuple()
-        epoch_seconds = long(shit_calendar.timegm(tt))
-        self.utc_millis = epoch_seconds*1000L+(dt.microsecond+500)/1000
+        epoch_seconds = shit_calendar.timegm(tt)
+        self.utc_micros = epoch_seconds*1000**2+dt.microsecond
 
     def set_timezone(self, timezone):
         return self.set_tz(timezone.zone)
@@ -63,8 +80,37 @@ class SaneTime(object):
         return self.new_tz(timezone.zone)
 
     def new_tz(self, tz):
-        return SaneTime(self.utc_millis, tz=tz)
+        return SaneTime(self.utc_micros, tz=tz)
 
     def to_datetime(self):
-        return pytz.utc.localize(fucked_datetime.utcfromtimestamp(self.utc_millis/1000)).astimezone(self.timezone)
+        dt = fucked_datetime.utcfromtimestamp(self.utc_micros/1000**2)
+        dt = pytz.utc.localize(dt)
+        dt = dt.replace(microsecond = self.utc_micros%1000**2)
+        dt = dt.astimezone(self.timezone)
+        return dt
+
+    def strftime(self, *args, **kwargs):
+        return self.to_datetime().strftime(*args, **kwargs)
+
+    def __lt__(self, other):
+        return self.utc_micros < other.utc_micros
+    def __le__(self, other):
+        return self.utc_micros <= other.utc_micros
+    def __gt__(self, other):
+        return self.utc_micros > other.utc_micros
+    def __ge__(self, other):
+        return self.utc_micros >= other.utc_micros
+
+    def __repr__(self):
+        return repr(self.to_datetime())
+    def __str__(self):
+        return str(self.to_datetime())
+
+    def _get_utc_seconds(self):
+        return (self.utc_micros+500000)/1000000
+    def _get_utc_millis(self):
+        return (self.utc_micros+500)/1000
+    utc_seconds = property(_get_utc_seconds)
+    utc_millis = property(_get_utc_millis)
+
 
--- sanetime/test/test_sanetime.py
+++ sanetime/test/test_sanetime.py
@@ -4,18 +4,17 @@ from sanetime.error import SaneTimeError
 from datetime import datetime
 import pytz
 
-# IMPORTANT-- you CANNOT use tzinfo on datetime-- you can mostly get it working if you associate it to pytz.utc, and then astimezone it to the correct timezone
-#   ONLY HAVE RESOLUTION TO SECONDS-- NO MILLIS UNFORTUNATELY IN PYTHON-- ONLY MICROSECONDS, but there are no utilities to convert
+# IMPORTANT -- note to self -- you CANNOT use tzinfo on datetime-- just pytz.timezone.localize everything to be safe
 
-JAN_MILLIS = 946684800000L
-JULY_MILLIS = 962409600000L
-HOUR_MILLIS = 1000L*60*60
+JAN_MICROS = 946684800*1000**2
+JULY_MICROS = 962409600*1000**2
+HOUR_MICROS = 60**2*1000**2
 
 class SaneTimeTest(unittest2.TestCase):
     """ A class that wraps all the date time timezone python insanity, so you don't have to know about that shitshow """
 
-    def assertInnards(self, utc_millis, timezone, st):
-        self.assertEquals(utc_millis, st.utc_millis)
+    def assertInnards(self, utc_micros, timezone, st):
+        self.assertEquals(utc_micros, st.utc_micros)
         self.assertEquals(timezone, st.timezone)
         self.assertEquals(timezone.zone, st.tz)
 
@@ -30,86 +29,158 @@ class SaneTimeTest(unittest2.TestCase):
         self.assertEquals('Africa/Cairo', self.ac.zone)
 
     def test_from_millis(self):
-        self.assertInnards(JAN_MILLIS, self.utc, SaneTime(JAN_MILLIS))
-        self.assertInnards(JAN_MILLIS, self.utc, SaneTime(JAN_MILLIS,tz='UTC'))
-        self.assertInnards(JAN_MILLIS, self.ny, SaneTime(JAN_MILLIS,tz='America/New_York'))
-        self.assertInnards(JAN_MILLIS, self.ny, SaneTime(JAN_MILLIS,timezone=self.ny))
+        self.assertInnards(JAN_MICROS, self.utc, SaneTime(JAN_MICROS))
+        self.assertInnards(JAN_MICROS, self.utc, SaneTime(JAN_MICROS,tz='UTC'))
+        self.assertInnards(JAN_MICROS, self.ny, SaneTime(JAN_MICROS,tz='America/New_York'))
+        self.assertInnards(JAN_MICROS, self.ny, SaneTime(JAN_MICROS,timezone=self.ny))
 
     def test_from_datetime_string(self):
-        self.assertInnards(JAN_MILLIS, self.utc, SaneTime('2000-01-01 00:00:00'))
-        self.assertInnards(JAN_MILLIS, self.utc, SaneTime('2000-01-01'))
-        self.assertInnards(JULY_MILLIS, self.utc, SaneTime('July 1st, 2000'))
+        self.assertInnards(JAN_MICROS, self.utc, SaneTime('2000-01-01 00:00:00'))
+        self.assertInnards(JAN_MICROS, self.utc, SaneTime('2000-01-01'))
+        self.assertInnards(JULY_MICROS, self.utc, SaneTime('July 1st, 2000'))
 
-        self.assertInnards(JAN_MILLIS, self.utc, SaneTime('2000-01-01 00:00:00', tz='UTC'))
-        self.assertInnards(JAN_MILLIS, self.utc, SaneTime('2000-01-01 00:00:00', timezone=self.utc))
+        self.assertInnards(JAN_MICROS, self.utc, SaneTime('2000-01-01 00:00:00', tz='UTC'))
+        self.assertInnards(JAN_MICROS, self.utc, SaneTime('2000-01-01 00:00:00', timezone=self.utc))
 
-        self.assertInnards(JAN_MILLIS+HOUR_MILLIS*5, self.ny, SaneTime('2000-01-01 00:00:00', tz='America/New_York'))
-        self.assertInnards(JAN_MILLIS+HOUR_MILLIS*5, self.ny, SaneTime('2000-01-01 00:00:00', timezone=self.ny))
+        self.assertInnards(JAN_MICROS+HOUR_MICROS*5, self.ny, SaneTime('2000-01-01 00:00:00', tz='America/New_York'))
+        self.assertInnards(JAN_MICROS+HOUR_MICROS*5, self.ny, SaneTime('2000-01-01 00:00:00', timezone=self.ny))
 
-        self.assertInnards(JULY_MILLIS+HOUR_MILLIS*4, self.ny, SaneTime('2000-07-01', tz='America/New_York'))
-        self.assertInnards(JULY_MILLIS+HOUR_MILLIS*4, self.ny, SaneTime('2000-07-01', timezone=self.ny))
+        self.assertInnards(JULY_MICROS+HOUR_MICROS*4, self.ny, SaneTime('2000-07-01', tz='America/New_York'))
+        self.assertInnards(JULY_MICROS+HOUR_MICROS*4, self.ny, SaneTime('2000-07-01', timezone=self.ny))
 
     def test_from_datetime_params(self):
-        self.assertInnards(JAN_MILLIS+60*60*1000L+60*1000L+1001L, self.utc, SaneTime(2000,1,1,1,1,1,1000))
-        self.assertInnards(JAN_MILLIS+60*60*1000L+60*1000L+1000L, self.utc, SaneTime(2000,1,1,1,1,1))
-        self.assertInnards(JAN_MILLIS+60*60*1000L+60*1000L, self.utc, SaneTime(2000,1,1,1,1))
-        self.assertInnards(JAN_MILLIS+60*60*1000L, self.utc, SaneTime(2000,1,1,1))
-        self.assertInnards(JAN_MILLIS, self.utc, SaneTime(2000,1,1))
-        self.assertInnards(JULY_MILLIS, self.utc, SaneTime(2000,7,1))
+        self.assertInnards(JAN_MICROS+60**2*1000**2+60*1000**2+1000**2+1, self.utc, SaneTime(2000,1,1,1,1,1,1))
+        self.assertInnards(JAN_MICROS+60**2*1000**2+60*1000**2+1000**2, self.utc, SaneTime(2000,1,1,1,1,1))
+        self.assertInnards(JAN_MICROS+60**2*1000**2+60*1000**2, self.utc, SaneTime(2000,1,1,1,1))
+        self.assertInnards(JAN_MICROS+60**2*1000**2, self.utc, SaneTime(2000,1,1,1))
+        self.assertInnards(JAN_MICROS, self.utc, SaneTime(2000,1,1))
+        self.assertInnards(JULY_MICROS, self.utc, SaneTime(2000,7,1))
         with self.assertRaises(SaneTimeError):
             SaneTime(2000,1)
 
-        self.assertInnards(JAN_MILLIS, self.utc, SaneTime(2000,1,1,0,0,0,0,tz='UTC'))
-        self.assertInnards(JAN_MILLIS, self.utc, SaneTime(2000,1,1,0,0,0,0,timezone=self.utc))
+        self.assertInnards(JAN_MICROS, self.utc, SaneTime(2000,1,1,0,0,0,0,tz='UTC'))
+        self.assertInnards(JAN_MICROS, self.utc, SaneTime(2000,1,1,0,0,0,0,timezone=self.utc))
 
-        self.assertInnards(JAN_MILLIS+HOUR_MILLIS*5, self.ny, SaneTime(2000,1,1,0,0,0,0,tz='America/New_York'))
-        self.assertInnards(JAN_MILLIS+HOUR_MILLIS*5, self.ny, SaneTime(2000,1,1,0,0,0,0,timezone=self.ny))
+        self.assertInnards(JAN_MICROS+HOUR_MICROS*5, self.ny, SaneTime(2000,1,1,0,0,0,0,tz='America/New_York'))
+        self.assertInnards(JAN_MICROS+HOUR_MICROS*5, self.ny, SaneTime(2000,1,1,0,0,0,0,timezone=self.ny))
 
-        self.assertInnards(JULY_MILLIS+HOUR_MILLIS*4, self.ny, SaneTime(2000,7,1, tz='America/New_York'))
-        self.assertInnards(JULY_MILLIS+HOUR_MILLIS*4, self.ny, SaneTime(2000,7,1, timezone=self.ny))
+        self.assertInnards(JULY_MICROS+HOUR_MICROS*4, self.ny, SaneTime(2000,7,1, tz='America/New_York'))
+        self.assertInnards(JULY_MICROS+HOUR_MICROS*4, self.ny, SaneTime(2000,7,1, timezone=self.ny))
 
     def test_from_datetime(self):
-        self.assertInnards(JAN_MILLIS, self.utc, SaneTime(datetime(2000,1,1)))
-        self.assertInnards(JAN_MILLIS, self.utc, SaneTime(datetime(2000,1,1, tzinfo=self.utc)))
-        self.assertInnards(JAN_MILLIS+HOUR_MILLIS*5, self.ny, SaneTime(self.ny.localize(datetime(2000,1,1))))
-        self.assertInnards(JULY_MILLIS+HOUR_MILLIS*4, self.ny, SaneTime(self.ny.localize(datetime(2000,7,1))))
+        self.assertInnards(JAN_MICROS, self.utc, SaneTime(datetime(2000,1,1)))
+        self.assertInnards(JAN_MICROS, self.utc, SaneTime(datetime(2000,1,1, tzinfo=self.utc)))
+        self.assertInnards(JAN_MICROS+HOUR_MICROS*5, self.ny, SaneTime(self.ny.localize(datetime(2000,1,1))))
+        self.assertInnards(JULY_MICROS+HOUR_MICROS*4, self.ny, SaneTime(self.ny.localize(datetime(2000,7,1))))
+
+    def test_now(self):
+        past = pytz.utc.localize(datetime.utcnow())
+        st = SaneTime()
+        future = pytz.utc.localize(datetime.utcnow())
+        self.assertTrue(st.to_datetime() > past)
+        self.assertTrue(st.to_datetime() < future)
+        self.assertEquals('UTC', st.tz)
+
+        past = self.utc.localize(datetime.utcnow()).astimezone(self.ny)
+        st = SaneTime(tz='America/New_York')
+        future = self.utc.localize(datetime.utcnow()).astimezone(self.ny)
+        self.assertTrue(st.to_datetime() > past)
+        self.assertTrue(st.to_datetime() < future)
+        self.assertEquals('America/New_York', st.tz)
+
+    def test_properties(self):
+        st = SaneTime(2000,1,2,3,4,5,600700)
+        self.assertEquals(700,st.utc_micros%1000)
+        self.assertEquals(601,st.utc_millis%1000)
+        self.assertEquals(6,st.utc_seconds%60)
+
+    def test_transitives(self):
+        st = SaneTime()
+        st2 = SaneTime(st.to_datetime())
+        self.assertEquals(st.utc_micros, st2.utc_micros)
+        self.assertEquals(st.tz, st2.tz)
+
+        st = SaneTime()
+        st2 = SaneTime(str(st))
+        self.assertEquals(st.utc_micros, st2.utc_micros)
+        self.assertEquals(st.tz, st2.tz)
+
+        st = SaneTime()
+        st2 = SaneTime(st.utc_micros)
+        self.assertEquals(st.utc_micros, st2.utc_micros)
+        self.assertEquals(st.tz, st2.tz)
+
+        st = SaneTime(tz='America/New_York')
+        st2 = SaneTime(st.to_datetime())
+        self.assertEquals(st.utc_micros, st2.utc_micros)
+        self.assertEquals(st.tz, st2.tz)
+
+        st = SaneTime(tz='America/New_York')
+        st2 = SaneTime(str(st),tz='America/New_York')
+        self.assertEquals(st.utc_micros, st2.utc_micros)
+        self.assertEquals(st.tz, st2.tz)
+
+        st = SaneTime(tz='America/New_York')
+        st2 = SaneTime(st.utc_micros,tz='America/New_York')
+        self.assertEquals(st.utc_micros, st2.utc_micros)
+        self.assertEquals(st.tz, st2.tz)
+
+    def test_comparisons(self):
+        t1 = SaneTime(2000,1,1,0,0,0,0)
+        t2 = SaneTime(2000,1,1,0,0,0,1)
+
+        self.assertFalse(t1 > t1)
+        self.assertTrue(t2 > t1)
+        self.assertFalse(t1 > t2)
+
+        self.assertTrue(t1 >= t1)
+        self.assertTrue(t2 >= t1)
+        self.assertFalse(t1 >= t2)
+
+        self.assertFalse(t1 < t1)
+        self.assertFalse(t2 < t1)
+        self.assertTrue(t1 < t2)
+
+        self.assertTrue(t1 <= t1)
+        self.assertFalse(t2 <= t1)
+        self.assertTrue(t1 <= t2)
 
     def test_construction_errors(self):
         with self.assertRaises(SaneTimeError):
-            SaneTime(JAN_MILLIS, tz='America/New_York', timezone=self.ny)
+            SaneTime(JAN_MICROS, tz='America/New_York', timezone=self.ny)
         with self.assertRaises(SaneTimeError):
-            SaneTime(JAN_MILLIS, tz='America/New_York', timezone=self.ac)
+            SaneTime(JAN_MICROS, tz='America/New_York', timezone=self.ac)
         with self.assertRaises(SaneTimeError):
             SaneTime(datetime(2000,1,1, tzinfo=self.ac), tz='America/New_York')
         with self.assertRaises(SaneTimeError):
             SaneTime(datetime(2000,1,1, tzinfo=self.ac), timzone=self.ny)
 
     def test_to_datetime(self):
-        st = SaneTime(JAN_MILLIS)
+        st = SaneTime(JAN_MICROS)
         self.assertEquals(datetime(2000,1,1,tzinfo=self.utc), st.to_datetime())
 
-        st = SaneTime(JAN_MILLIS, tz='America/New_York')
+        st = SaneTime(JAN_MICROS, tz='America/New_York')
         self.assertEquals(datetime(2000,1,1,tzinfo=self.utc).astimezone(self.ny), st.to_datetime())
 
-        st = SaneTime(JULY_MILLIS, tz='America/New_York')
+        st = SaneTime(JULY_MICROS, tz='America/New_York')
         self.assertEquals(datetime(2000,7,1,tzinfo=self.utc).astimezone(self.ny), st.to_datetime())
 
     def test_timezone_switch(self):
-        st = SaneTime(JAN_MILLIS)
-        self.assertInnards(JAN_MILLIS, self.ny, st.set_tz('America/New_York'))
-        self.assertInnards(JAN_MILLIS, self.ny, st) # make sure it operates on itself
+        st = SaneTime(JAN_MICROS)
+        self.assertInnards(JAN_MICROS, self.ny, st.set_tz('America/New_York'))
+        self.assertInnards(JAN_MICROS, self.ny, st) # make sure it operates on itself
 
-        st = SaneTime(JAN_MILLIS)
-        self.assertInnards(JAN_MILLIS, self.ny, st.set_timezone(self.ny)) # make sure it's returning an obj
-        self.assertInnards(JAN_MILLIS, self.ny, st) # make sure it operates on itself
+        st = SaneTime(JAN_MICROS)
+        self.assertInnards(JAN_MICROS, self.ny, st.set_timezone(self.ny)) # make sure it's returning an obj
+        self.assertInnards(JAN_MICROS, self.ny, st) # make sure it operates on itself
 
-        st = SaneTime(JULY_MILLIS)
-        self.assertInnards(JULY_MILLIS, self.ny, st.new_tz('America/New_York'))
-        self.assertInnards(JULY_MILLIS, self.utc, st) # make sure it doesn't operates on itself
+        st = SaneTime(JULY_MICROS)
+        self.assertInnards(JULY_MICROS, self.ny, st.new_tz('America/New_York'))
+        self.assertInnards(JULY_MICROS, self.utc, st) # make sure it doesn't operates on itself
 
-        st = SaneTime(JULY_MILLIS)
-        self.assertInnards(JULY_MILLIS, self.ny, st.new_timezone(self.ny)) # make sure it's returning an obj
-        self.assertInnards(JULY_MILLIS, self.utc, st) # make sure it doesn't operates on itself
+        st = SaneTime(JULY_MICROS)
+        self.assertInnards(JULY_MICROS, self.ny, st.new_timezone(self.ny)) # make sure it's returning an obj
+        self.assertInnards(JULY_MICROS, self.utc, st) # make sure it doesn't operates on itself
 
 
 if __name__ == '__main__':
