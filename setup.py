3f7dc503a0ddf6023ddd5b9973b5d4f76fa1f980
prior <prior@cracklabs.com>
2011-10-23 19:12:24 -0400

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='sanetime',
-    version='1.0.1',
+    version='2.0.0',
     description='A Sane date/time/timezone/dateutil/calendar/pytz Wrapping Time Class',
     author='Michael Prior',
     author_email='prior@cracklabs.com',
