6c310d8d79a1263194f4b8dcd7573b9bb4ee9b94
prior <prior@cracklabs.com>
2011-10-28 16:12:14 -0400

    bump version and remove django dependencies

--- setup.py
+++ setup.py
@@ -3,19 +3,18 @@ from distutils.core import setup
 
 setup(
     name='sanetime',
-    version='2.0.1',
+    version='3.0.0',
     description='A drop-in replacement for datetime that bring sanity and ease-of-use to the date/time/timezone/dateutil/calendar/pytz/timetuple/parsing insane shitshow.',
     author='Michael Prior',
     author_email='prior@cracklabs.com',
     url='https://github.com/prior/sanetime',
-    download_url='https://github.com/prior/sanetime/tarball/v2.0.1',
+    download_url='https://github.com/prior/sanetime/tarball/v3.0.0',
     packages=['sanetime'],
     install_requires=[
         'nose',
         'pytz',
         'unittest2',
-        'python-dateutil==1.5',
-        'Django==1.3.1'
+        'python-dateutil==1.5'
     ],
     long_description="""
 
