9800e00694fbebc8452bd4deced52f9a94d5a2a7
prior <prior@cracklabs.com>
2011-11-01 10:31:38 -0400

    make more cheeseshop friendly

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-sanetime
-========
+sanetime and sanetztime
+=======================
 
-A sane wrapper around python's date / time / datetime / dateutils / pytz / calendar / timetuple / timezone / timestamp insanity
+Sane wrappers around the python's datetime / time / date / timetuple / pytz / timezone / calendar /
+timedelta / utc shitshow.  This takes care of most of the ridiculous shit so you don't have to
+flush precious brain cells down the toilet trying to figure all this out.  You owe me a beer.  At
+least.
 
+There are two classes that you may find useful here, and you should understand the difference:
+
+* sanetime:  this class is only concerned with a particular moment in time, NOT where that moment
+        was experienced.
+* sanetztime:  this class is concerned with a particular moment in time AND in what timezone that
+        moment was experienced.
 
--- setup.py
+++ setup.py
@@ -4,27 +4,18 @@ from distutils.core import setup
 setup(
     name='sanetime',
     version='3.0.2',
-    description='A drop-in replacement for datetime that bring sanity and ease-of-use to the date/time/timezone/dateutil/calendar/pytz/timetuple/parsing insane shitshow.',
+    description='A drop-in replacement for datetime that brings sanity and ease-of-use to the date/time/timezone/dateutil/calendar/pytz/timetuple/parsing insane shitshow.',
+    long_description = open('README.md').read(),
     author='Michael Prior',
     author_email='prior@cracklabs.com',
     url='https://github.com/prior/sanetime',
     download_url='https://github.com/prior/sanetime/tarball/v3.0.2',
+    license='LICENSE.txt',
     packages=['sanetime'],
     install_requires=[
         'nose',
         'pytz',
         'unittest2',
         'python-dateutil==1.5'
-    ],
-    long_description="""
-
-        A drop-in replacement for datetime that bring sanity and ease-of-use to the date/time/timezone/dateutil/calendar/pytz/timetuple/parsing insane shitshow.
-
-        This library forces timezones on every object, and stores everything in UTC micros internally.  Expect ease-of-use and correctness in all aspects of datetime/timezone manipulation now.
-
-        Maintained on github:
-            https://github.com/prior/sanetime
-
-    """
-
+    ]
 )
