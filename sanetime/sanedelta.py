a1a7c3c800f6df814bfee3e5b7be65b106403cef
prior <prior@cracklabs.com>
2012-04-09 20:43:58 -0400

    added avg_year and avg_month to delta possiblities

--- sanetime/sanedelta.py
+++ sanetime/sanedelta.py
@@ -4,6 +4,8 @@ MINUTE_MICROS = SECOND_MICROS * 60
 HOUR_MICROS = MINUTE_MICROS * 60
 DAY_MICROS = HOUR_MICROS * 24
 WEEK_MICROS = DAY_MICROS * 7
+AVG_MONTH_MICROS = (DAY_MICROS * 365*4+1)/(12*4)
+AVG_YEAR_MICROS = (DAY_MICROS * 365*4+1)/4
 
 HALF_MILLI_MICROS = MILLI_MICROS / 2
 HALF_SECOND_MICROS = SECOND_MICROS / 2
@@ -11,13 +13,17 @@ HALF_MINUTE_MICROS = MINUTE_MICROS / 2
 HALF_HOUR_MICROS = HOUR_MICROS / 2
 HALF_DAY_MICROS = DAY_MICROS / 2
 HALF_WEEK_MICROS = WEEK_MICROS / 2
+HALF_AVG_MONTH_MICROS = AVG_MONTH_MICROS / 2
+HALF_AVG_YEAR_MICROS = AVG_YEAR_MICROS / 2
 
 TRANSLATIONS = (
+        (('ay','avg_years'),AVG_YEAR_MICROS),
+        (('am','avg_months'),WEEK_MICROS),
         (('w','weeks'),WEEK_MICROS),
         (('d','days'),DAY_MICROS),
         (('h','hours'),HOUR_MICROS),
-        (('m','minutes'),MINUTE_MICROS),
-        (('s','seconds'),SECOND_MICROS),
+        (('m','mins','minutes'),MINUTE_MICROS),
+        (('s','secs','seconds'),SECOND_MICROS),
         (('ms','millis','milli_seconds'),MILLI_MICROS),
         (('us','micros','micro_seconds'),1) )
 TRANSLATION_HASH = dict((alt,v) for k,v in TRANSLATIONS for alt in k)
@@ -42,6 +48,10 @@ class SaneDelta(object):
     def d(self): return (self.us + HALF_DAY_MICROS) / DAY_MICROS
     @property
     def w(self): return (self.us + HALF_WEEK_MICROS) / WEEK_MICROS
+    @property
+    def am(self): return (self.us + HALF_AVG_MONTH_MICROS) / AVG_MONTH_MICROS
+    @property
+    def ay(self): return (self.us + HALF_AVG_YEAR_MICROS) / AVG_YEAR_MICROS
 
     # aliases
     @property
@@ -50,10 +60,14 @@ class SaneDelta(object):
     millis = ms
     milliseconds = ms
     seconds = s
+    secs = s
     minutes = m
+    mins = m
     hours = h
     days = d
     weeks = w
+    avg_months = am
+    avg_years = ay
 
     # unrounded amounts
     @property
@@ -70,6 +84,10 @@ class SaneDelta(object):
     def _d(self): return self.us / DAY_MICROS
     @property
     def _w(self): return self.us / WEEK_MICROS
+    @property
+    def _am(self): return self.us / AVG_MONTH_MICROS
+    @property
+    def _ay(self): return self.us / AVG_YEAR_MICROS
 
     # aliases
     _micros = _us
@@ -77,10 +95,14 @@ class SaneDelta(object):
     _millis = _ms
     _milliseconds = _ms
     _seconds = _s
+    _secs = _s
     _minutes = _m
+    _mins = _m
     _hours = _h
     _days = _d
     _weeks = _w
+    _avg_months = _am
+    _avg_years = _ay
 
     # float amounts
     @property
@@ -97,6 +119,10 @@ class SaneDelta(object):
     def fd(self): return float(self.us) / DAY_MICROS
     @property
     def fw(self): return float(self.us) / WEEK_MICROS
+    @property
+    def fam(self): return float(self.us) / AVG_MONTH_MICROS
+    @property
+    def fay(self): return float(self.us) / AVG_YEAR_MICROS
 
     # aliases
     f_micros = fus
@@ -104,10 +130,14 @@ class SaneDelta(object):
     f_millis = fms
     f_milliseconds = fms
     f_seconds = fs
+    f_secs = fs
     f_minutes = fm
+    f_mins = fm
     f_hours = fh
     f_days = fd
     f_weeks = fw
+    f_avg_months = fam
+    f_avg_years = fay
 
 
     def __lt__(self, other): return self.us < int(other)
--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 
 setup(
     name='sanetime',
-    version='3.4.3',
+    version='3.5.0',
     description='A drop-in replacement for datetime that brings sanity and ease-of-use to the date/time/timezone/dateutil/calendar/pytz/timetuple/parsing insane shitshow.',
     long_description = open('README.md').read(),
     author='Michael Prior',
     author_email='prior@cracklabs.com',
     url='https://github.com/prior/sanetime',
-    download_url='https://github.com/prior/sanetime/tarball/v3.4.3',
+    download_url='https://github.com/prior/sanetime/tarball/v3.5.0',
     license='LICENSE.txt',
     packages=['sanetime'],
     install_requires=[
