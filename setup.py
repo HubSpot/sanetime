ecbb4f237bbef10d18fc5d2cd98e8d71a90bceb0
prior <prior@cracklabs.com>
2012-05-04 01:41:09 -0400

    getting ready for pypi

--- setup.py
+++ setup.py
@@ -6,13 +6,19 @@ VERSION = '4.0.1'
 setup(
     name='sanetime',
     version=VERSION,
-    description='A drop-in replacement for datetime that brings sanity and ease-of-use to the date/time/timezone/dateutil/calendar/pytz/timetuple/parsing insane shitshow.',
-    long_description = open('README.md').read(),
     author='Michael Prior',
     author_email='prior@cracklabs.com',
+    packages=['sanetime'],
     url='https://github.com/prior/sanetime',
     download_url='https://github.com/prior/sanetime/tarball/v%s'%VERSION,
     license='LICENSE.txt',
-    packages=['sanetime'],
-    install_requires=[ 'nose==1.1.2', 'pytz==2012b', 'python-dateutil==1.5' ]
+    description='A sane date/time python interface -- better epoch time, timezone, and delta support -- optional django support as well',
+    long_description=open('README.rst').read(),
+    install_requires=[ 
+        'nose==1.1.2', 
+        'pytz>=2012b', 
+        'python-dateutil==1.5' 
+    ]
 )
+
+
