8416f8b0d7081e93b5b07b0db003be3ec1c958c3
prior <prior@cracklabs.com>
2012-05-04 16:52:08 -0400

    helper script for new version publishing

---
+++ new_version.py
@@ -0,0 +1,19 @@
+import subprocess
+
+def call(args):
+    cmd = ' '.join(args)
+    print("== attempting == %s =="%cmd)
+    if subprocess.call(args):
+        print("== !problem!  == %s ==" % cmd)
+        exit(1)
+    print("== done       == %s ==" % cmd)
+    print
+
+version = [line for line in open('setup.py').read().split('\n') if line.startswith('VERSION = ')][0].split(' ')[-1].strip('"').strip("'")
+
+call(['git', 'commit', '-v'])
+call(['git', 'push'])
+call(['git', 'tag', '-a', 'v%s'%version, '-m="version bump"'])
+call(['git', 'push', '--tags'])
+call(['python', 'setup.py', 'sdist', 'upload'])
+
