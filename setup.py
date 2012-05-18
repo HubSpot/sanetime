079a6ad37c2b9a619aae166a7db27fd0e2d8c752
prior <prior@cracklabs.com>
2012-05-18 00:45:59 -0400

    clean up dependencies

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-VERSION = '4.0.7'
+VERSION = '4.0.8'
 
 setup(
     name='sanetime',
     version=VERSION,
-    author='Michael Prior',
-    author_email='prior@cracklabs.com',
-    packages=['sanetime'],
-    url='https://github.com/prior/sanetime',
-    download_url='https://github.com/prior/sanetime/tarball/v%s'%VERSION,
+    author='prior',
+    author_email='mprior@hubspot.com',
+    packages=find_packages(),
+    url='https://github.com/HubSpot/sanetime',
+    download_url='https://github.com/HubSpot/sanetime/tarball/v%s'%VERSION,
     license='LICENSE.txt',
-    description='A sane date/time python interface -- better epoch time, timezones, and deltas -- django support as well',
+    description='A sane date/time python interface:  better epoch time, timezones, and deltas -- django support as well',
     long_description=open('README.rst').read(),
     install_requires=[
         'pytz>=2010',
         'python-dateutil>=1.5,<2.0'  # we're not compatible with python 3.0 yet
-    ]
+    ],
+    platforms=['any']
 )
-
 
