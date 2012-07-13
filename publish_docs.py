f95e47e24d3cac603be191846b008c45790a151c
prior <mprior@hubspot.com>
2012-07-13 16:59:14 -0400

    testing

--- publish_docs.py
+++ publish_docs.py
@@ -4,21 +4,21 @@ import subprocess
 def call(args):
     cmd = ' '.join(args)
     print("== attempting == %s =="%cmd)
-    if subprocess.call(args):
+    if subprocess.call(args, shell=True):
         print("== !problem!  == %s ==" % cmd)
         exit(1)
     print("== done       == %s ==" % cmd)
     print
 
-call(['git', 'checkout', 'master'], shell=True)
-call(['make', '-C', 'docs', 'html'], shell=True)
-call(['rm', '-rf', '/tmp/docs_html'], shell=True)
-call(['cp', '-R', 'docs/_build/html', '/tmp/docs_html'], shell=True)
-call(['git', 'checkout', 'gh-pages'], shell=True)
-call(['rm', '-rf', 'docs'], shell=True)  # wtf!!  i can't do a rm -rf * for some reason
-call(['rm', '-rf', 'sanetime'], shell=True)  # wtf!!  i can't do a rm -rf * for some reason
-call(['cp', '-R', '/tmp/docs_html/*', '.'], shell=True)
-call(['git', 'add', '.'], shell=True)
+call(['git', 'checkout', 'master'])
+call(['make', '-C', 'docs', 'html'])
+call(['rm', '-rf', '/tmp/docs_html'])
+call(['cp', '-R', 'docs/_build/html', '/tmp/docs_html'])
+call(['git', 'checkout', 'gh-pages'])
+call(['rm', '-rf', 'docs'])  # wtf!!  i can't do a rm -rf * for some reason
+call(['rm', '-rf', 'sanetime'])  # wtf!!  i can't do a rm -rf * for some reason
+call(['cp', '-R', '/tmp/docs_html/*', '.'])
+call(['git', 'add', '.'])
 #call(['git', 'commit', '-a', '-v'])
 #call(['git', 'push'])
 #call(['git', 'checkout', 'master'])
